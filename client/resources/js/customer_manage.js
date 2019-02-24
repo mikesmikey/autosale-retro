2385be18bc24ad3a6d8096b7972f75d744827e67
bookploy1928 <bookploy1928@hotmail.com>
2019-02-24 19:24:03 +0700

    customerV2

--- client/resources/js/customer_manage.js
+++ client/resources/js/customer_manage.js
@@ -1,7 +1,15 @@
-function launchCustomer() {
+function launchCustomerOrder() {
     document.getElementById('order-customer').classList.add('is-active');
 }
 
-function closeCustomer() {
+function closeCustomerOrder() {
     document.getElementById('order-customer').classList.remove('is-active');
+}
+
+function launchCustomerDelete() {
+    document.getElementById('delete-customer').classList.add('is-active');
+}
+
+function closeCustomerDelete() {
+    document.getElementById('delete-customer').classList.remove('is-active');
 }
