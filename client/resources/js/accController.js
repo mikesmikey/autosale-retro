3cb0d6ad8f1a463630ca096e1a324cbce9278a57
bookploy1928 <bookploy1928@hotmail.com>
2019-02-26 13:03:59 +0700

    sadf

--- client/resources/js/accController.js
+++ client/resources/js/accController.js
@@ -24,23 +24,13 @@ function refreshPage() {
     window.location.reload();
 }
 
-<<<<<<< HEAD
-=======
-function plate_license_add() {
-    window.location.href = './plate_license_add.html';
-}
 
-function mockCarFixAdd() {
-    window.location.href = './car_fix_add.html';
-}
 
-function usedPartUpdate() {
-    window.location.href = './used_part_update.html';
-}
+
+
 
 
 
 
 //initial method
 checkAuth();
->>>>>>> a412a086b6c02161e2778e214d4d057d9155464f
