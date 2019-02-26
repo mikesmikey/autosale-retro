fbf657f47847a9fff2d803cdd04392b16b161d2c
heroserth2 <59160969@go.buu.ac.th>
2019-02-26 12:55:05 +0700

    fin plate

---
+++ client/resources/js/Controller_page.js
@@ -0,0 +1,16 @@
+
+function plate_license_add() {
+    window.location.href = './plate_license_add.html';
+}
+function plate_license() {
+    window.location.href = './plate_license.html';
+}
+function mockCarFixAdd() {
+    window.location.href = './car_fix_add.html';
+}
+function usedPartUpdate() {
+    window.location.href = './used_part_update.html';
+}
+function mockCarbuy2() {
+    window.location.replace('./car_buy2.html');
+}
