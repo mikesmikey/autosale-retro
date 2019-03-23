36bce76b8a6e91ba86c2f892b916da519c631bb5
59160927 <59160927@go.buu.ac.th>
2019-03-23 06:22:22 -0700

    new

--- client/resources/js/partner_manage.js
+++ client/resources/js/partner_manage.js
@@ -1,3 +1,8 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+const url = 'mongodb://hanami:hanami02@ds131765.mlab.com:31765/ooad_kob';
+const dbName = "ooad_kob";
+
 function launchPartnerEdit() {
     document.getElementById('edit-Partner').classList.add('is-active');
 }
@@ -10,7 +15,7 @@ function launchPartnerAdd() {
     document.getElementById('add-Partner').classList.add('is-active');
 }
 
-function closePartnerAdd() {
+function closePartnerAdd()  {
     document.getElementById('add-Partner').classList.remove('is-active');
 }
 
@@ -21,14 +26,16 @@ function launchPartnerDelete() {
 function closePartnerDelete() {
     document.getElementById('delete-Partner').classList.remove('is-active');
 }
-function handleInputChange(e) {
-    const name = target.name;
-    const value = target.value;
 
-    this.setState({
-        [name]: value
-    })
+function clearInput(companyName,partnerName,partnerPhone,partnerType,partnerAddr) {
+    companyName = "";
+    partnerName = "";
+    partnerPhone = "";
+    partnerType = "";
+    partnerAddr = "";
+    return(companyName,partnerName,partnerPhone,partnerType,partnerAddr);
 }
+
 function addButtonHandle(companyName,partnerName,partnerPhone,partnerType,partnerAddr) {
     var newData = {};
     newData.companyName = companyName;
@@ -42,5 +49,13 @@ function addButtonHandle(companyName,partnerName,partnerPhone,partnerType,partne
     console.log(partnerPhone);
     console.log(partnerType);
     console.log(partnerAddr);
+    insertPartner(newData);
     //CServiceObj.addUser(newData)
+}
+
+function insertPartner(newData) {
+        MongoClient.connect(url, function(err, client) {
+            const db = client.db(dbName)
+            db.collection('Partner').insertOne({newData});
+        });
 }
