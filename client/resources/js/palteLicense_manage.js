4d8d4236213a618c7f5203142d7d6dc54bb5e4cb
heroserth2 <59160969@go.buu.ac.th>
2019-03-25 10:35:28 +0700

    finish serach from license plate

--- client/resources/js/palteLicense_manage.js
+++ client/resources/js/palteLicense_manage.js
@@ -39,6 +39,7 @@ function searchProduct(nameKey, myArray) {
             return myArray[i];
         }
     }
+    return null;
 }
 function searchCustomerName(nameKey, myArray) {
     for (var i = 0; i < myArray.length; i++) {
@@ -52,15 +53,15 @@ function ShowDetail(value) {
     console.log(product)
     let resultObject = searchProduct(value, product);
     console.log(resultObject)
-    document.getElementById("dca_prod_id").innerHTML = "เลขออเดอร์ : "+resultObject.prod_id;
+    document.getElementById("dca_prod_id").innerHTML = "เลขออเดอร์ : " + resultObject.prod_id;
     //เลขทะเบียนรถ
-    document.getElementById("dca_car_license").innerHTML = "เลขทะเบียน : "+resultObject.trn_car.car_license;
+    document.getElementById("dca_car_license").innerHTML = "เลขทะเบียน : " + resultObject.trn_car.car_license;
     //ยี่ห้อ
-    document.getElementById("dca_car_brand").innerHTML = "ยี่ห้อ : "+resultObject.trn_car.car_brand;
+    document.getElementById("dca_car_brand").innerHTML = "ยี่ห้อ : " + resultObject.trn_car.car_brand;
     //รุ่น
-    document.getElementById("dca_car_model").innerHTML = "รุ่น : "+resultObject.trn_car.car_model;
+    document.getElementById("dca_car_model").innerHTML = "รุ่น : " + resultObject.trn_car.car_model;
     //เจ้าของ
-    document.getElementById("dca_customer_name").innerHTML = "เจ้าของ : "+ searchCustomerName(resultObject.cust_id, customer);
+    document.getElementById("dca_customer_name").innerHTML = "เจ้าของ : " + searchCustomerName(resultObject.cust_id, customer);
 }
 function createselect(data) {
     for (let i = 0; i < data.length; i++) {
@@ -68,6 +69,7 @@ function createselect(data) {
         var option = document.createElement("option");
         option.text = data[i].trn_car.car_license;
         option.value = data[i].trn_car.car_license;
+        console.log(data[i].trn_car.car_license)
         option.onclick = function () { ShowDetail(this.value); };
         select.add(option);
     }
@@ -78,6 +80,52 @@ function startForm() {
     });
     getAllCustomer();
 }
+
+function runScript(e) {
+    if (e.keyCode == 13) {
+        var txt = document.getElementById("input_car_license").value
+        if (txt === "") {
+            removeAlloption();
+            createselect(product);
+        } else {
+            let resultObject = searchProduct(txt, product);
+            if(resultObject !== null){
+                removeAlloption();
+                var select = document.getElementById("selectNumber");
+                var option = document.createElement("option");
+                option.text = resultObject.trn_car.car_license;
+                option.value = resultObject.trn_car.car_license;
+                option.onclick = function () { ShowDetail(this.value); };
+                ShowDetail(resultObject.trn_car.car_license)
+                select.add(option);
+            }else{
+                removeAlloption();
+            }
+        }
+    }
+    return false;
+}
+function removeAlloption() {
+    var select = document.getElementById("selectNumber");
+    var length = select.options.length;
+    for (i = 0,c=0 ; i < length; i++) {
+        select.options[c] = null;
+    }
+}
+/*
+document.getElementById("input_car_license").addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+       var txt = document.getElementById("input_car_license").value
+        if(txt === ""){
+            createselect(product);
+        }else{
+            let resultObject = searchProduct(value, product);
+            createselect(resultObject)
+        }
+     //event.preventDefault();
+    
+    }
+  });*/
 
 startForm();
 
