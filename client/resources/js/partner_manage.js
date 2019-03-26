8f44cc46cdecf8cd562e24bc45e993111dfb4e1b
59160927 <59160927@go.buu.ac.th>
2019-03-25 23:06:58 -0700

    new

--- client/resources/js/partner_manage.js
+++ client/resources/js/partner_manage.js
@@ -1,7 +1,6 @@
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectID;
-const url = 'mongodb://hanami:hanami02@ds131765.mlab.com:31765/ooad_kob';
-const dbName = "ooad_kob";
+var partner = [];
+var select = document.getElementById("PartnerSearch");
+let max = 0;
 
 function launchPartnerEdit() {
     document.getElementById('edit-Partner').classList.add('is-active');
@@ -16,7 +15,9 @@ function launchPartnerAdd() {
 }
 
 function closePartnerAdd()  {
+    console.log(partner);
     document.getElementById('add-Partner').classList.remove('is-active');
+    document.getElementById('add-Partner').reset();
 }
 
 function launchPartnerDelete() {
@@ -36,26 +37,101 @@ function clearInput(companyName,partnerName,partnerPhone,partnerType,partnerAddr
     return(companyName,partnerName,partnerPhone,partnerType,partnerAddr);
 }
 
+function setSelected(data) {
+    let min = data[0].partner_id;
+    let first = 0;
+    for(let i = 0;i<data.length;i++){
+        var el = document.createElement("option");
+        el.value = data[i].company_name;
+        el.textContent = data[i].company_name;
+        if(data[i].partner_id < min){
+            el.selected = true;
+            min=data[i].partner_id;
+            first = i;
+        }
+        if(data[i].partner_id > max ){
+            max=data[i].partner_id;
+        }
+        select.appendChild(el);
+        partner.push(data[i]);
+       // console.log(partner[i])
+    }
+    document.getElementById("companyName").innerHTML = partner[first].company_name;
+    document.getElementById("partnerName").innerHTML = partner[first].partner_name;
+    document.getElementById("partnerPhone").innerHTML = partner[first].partner_phone;
+    document.getElementById("partnerType").innerHTML = partner[first].partner_type;
+    document.getElementById("partnerAddr").innerHTML = partner[first].partner_addr;
+}
+
+
+function getAllPartner() {
+    return new Promise((resolve, reject) => {
+            axios.get('http://localhost:5000/partners').then((result) => {
+                resolve(result.data);
+            })
+    })
+}
+
+getAllPartner().then((data) => {
+    setSelected(data);
+})
+
+
+function addPartner(partnerData) {
+    return new Promise((resolve, reject) => {
+        axios.post('http://localhost:5000/partners/add', {"partnerData" : partnerData}).then((result) => {
+            resolve(result.data);
+        })
+    })
+}
+
+
 function addButtonHandle(companyName,partnerName,partnerPhone,partnerType,partnerAddr) {
-    var newData = {};
-    newData.companyName = companyName;
-    newData.partnerName = partnerName;
-    newData.partnerPhone = partnerPhone;
-    newData.partnerType = partnerType;
-    newData.partnerAddr = partnerAddr;
+    var partnerData = {};
+    partnerData.partner_id = max+1;
+    partnerData.company_name = companyName;
+    partnerData.partner_name = partnerName;
+    partnerData.partner_phone = partnerPhone;
+    partnerData.partner_type = partnerType;
+    partnerData.partner_addr = partnerAddr;
 
-    console.log(companyName);
-    console.log(partnerName);
-    console.log(partnerPhone);
-    console.log(partnerType);
-    console.log(partnerAddr);
-    insertPartner(newData);
-    //CServiceObj.addUser(newData)
+    console.log(partnerData);
+    addPartner(partnerData).then((data) => {
+        if (data) {
+            alert("สำเร็จ!")
+            var el = document.createElement("option");
+            el.value = partnerData.company_name;
+            el.textContent = partnerData.company_name;
+            select.appendChild(el);
+            partner.push(partnerData);
+            closePartnerAdd();
+        } else {
+            alert("เพิ่มไม่สำเร็จ!")
+            closePartnerAdd();
+        }
+    })
 }
 
-function insertPartner(newData) {
-        MongoClient.connect(url, function(err, client) {
-            const db = client.db(dbName)
-            db.collection('Partner').insertOne({newData});
-        });
+
+function loadDetailPartner() {
+    for(let i = 0;i<partner.length;i++){
+        if(partner[i].company_name == document.getElementById("PartnerSearch").value){
+            document.getElementById("companyName").innerHTML = partner[i].company_name;
+            document.getElementById("partnerName").innerHTML = partner[i].partner_name;
+            document.getElementById("partnerPhone").innerHTML = partner[i].partner_phone;
+            document.getElementById("partnerType").innerHTML = partner[i].partner_type;
+            document.getElementById("partnerAddr").innerHTML = partner[i].partner_addr;
+            break;
+        }
+        //console.log(partner[i])
+    }
+    //document.getElementById("label").innerHTML=document.getElementById("PartnerSearch").value;
 }
+
+
+
+
+
+
+
+
